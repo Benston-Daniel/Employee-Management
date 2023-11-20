6e5b0f78b0fe666f66b35b3d11034d5cb9ef3f49
BENSTON DANIEL <110040264+Benston-Daniel@users.noreply.github.com>
2023-11-20 16:28:50 +0530

    Update server.js

--- em-backend/server.js
+++ em-backend/server.js
@@ -12,7 +12,6 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-// Endpoint to get all employees
 app.get('/employees', async (req, res) => {
     try {
         const data = await fs.readFile('employees.json', 'utf-8');
@@ -23,25 +22,21 @@ app.get('/employees', async (req, res) => {
     }
 });
 
-// Function to read and parse employees.json
 async function readEmployeesFile() {
     const data = await fs.readFile('employees.json', 'utf-8');
     return JSON.parse(data);
 }
 
-// Function to write employees to employees.json
 async function writeEmployeesFile(employees) {
     await fs.writeFile('employees.json', JSON.stringify(employees, null, 2));
 }
 
-// Endpoint to add a new employee
 app.post('/employees', async (req, res) => {
     try {
         console.log("Inside Post");
         const newEmployee = req.body;
         const employees = await readEmployeesFile();
 
-        // Assign a new ID and increment the last used ID
         newEmployee.id = (parseInt(employees[employees.length - 1].id) + 1).toString();
 
         employees.push(newEmployee);
@@ -53,7 +48,6 @@ app.post('/employees', async (req, res) => {
     }
 });
 
-// Endpoint to delete an existing employee
 app.delete('/employees/:id', async (req, res) => {
     try {
         const employeeId = req.params.id;
@@ -62,7 +56,6 @@ app.delete('/employees/:id', async (req, res) => {
         const index = employees.findIndex((employee) => employee.id === employeeId);
 
         if (index !== -1) {
-            // Remove the employee and decrement the last used ID
             employees.splice(index, 1);
             await writeEmployeesFile(employees);
             res.json({ success: true });
@@ -73,8 +66,6 @@ app.delete('/employees/:id', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-// ... (Other endpoints remain unchanged)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
